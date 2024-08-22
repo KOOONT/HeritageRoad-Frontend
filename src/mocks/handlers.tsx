12fad4cf9c46115cb77610266975e2ccbb83b864
yujinjeon12 <tamittu@naver.com>
2024-08-22 18:33:07 +0900

    Add time delay passthrough handler #5

--- src/mocks/handlers.tsx
+++ src/mocks/handlers.tsx
@@ -1,7 +1,11 @@
-import { http, HttpResponse } from 'msw'
+import { http, HttpResponse, delay } from 'msw'
 import markers from './dummy/markers.json';
 
 export const handlers = [
+    //add time delay passthrough handler
+    http.all('*', async () => {
+      await delay(1000)
+    }),
     http.get('http://localhost/search/heritage', () => {
       // Construct a JSON response with the list of all posts
       // as the response body.
